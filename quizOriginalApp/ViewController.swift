59ae181fe50a788cb122cb5fe28d1c6917d026fb
Ryo Motoki <lalalararirureryo@Ryo-no-MacBook-Air.local>
2017-02-22 16:26:47 +0900

    Initial Commit

---
+++ quizOriginalApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quizOriginalApp
+//
+//  Created by Ryo Motoki on 2017/02/22.
+//  Copyright © 2017年 Desital Curcus Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
